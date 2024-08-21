d145f8247f01bd1d8e41cca41c44683f175bfad5
Olivier Boudry <79530946+oboudry-mvp@users.noreply.github.com>
2024-08-21 15:02:20 +0200

    Update ask_hr_policies_pipeline.py
    Change the response length directive.

--- ask_hr_policies_pipeline.py
+++ ask_hr_policies_pipeline.py
@@ -73,8 +73,7 @@ class Pipeline:
             "You are an assistant for question-answering tasks. "
             "Use the following pieces of retrieved context to answer "
             "the question. If you don't know the answer, say that you "
-            "don't know. Use three sentences maximum and keep the "
-            "answer concise."
+            "don't know. Keep the answer concise."
             "\\n\\n"
             "{context}"
         )
