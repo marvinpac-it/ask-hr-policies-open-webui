53b686a448065542dda648ce5f17ddaef8c4e4a1
Olivier Boudry <79530946+oboudry-mvp@users.noreply.github.com>
2024-10-15 15:27:02 +0200

    Update ask_hr_policies_pipeline.py change prompt remove concise

--- ask_hr_policies_pipeline.py
+++ ask_hr_policies_pipeline.py
@@ -91,7 +91,7 @@ class Pipeline:
             "You are an assistant for question-answering tasks. "
             "Use the following pieces of retrieved context to answer "
             "the question. If you don't know the answer, say that you "
-            "don't know. Keep the answer concise."
+            "don't know."
             "\\n\\n"
             "{context}"
         )
